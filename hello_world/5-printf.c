5083a582b52f9a6c87fff8441e97cc07e0edd5d7
jsebasgr21 <jsebasgr21@hotmail.com>
2023-10-26 19:39:24 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -9,8 +9,8 @@
 
 #include <stdio.h>
 
-int main()
-{
-        printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{	
+	printf("with proper grammar, but the outcome is a piece of art,");
         return (0);
 }
