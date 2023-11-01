ddbc3dcc819ac4f8fadc35455d2dbcb735290372
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 11:22:20 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - del uno al diez 
+ *
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a < '10'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+	
