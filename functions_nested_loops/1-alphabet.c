90edf6aa9fc3dd9fddfe6d6586b4e28addc0a915
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 14:42:53 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+int print_alphabet(void)
+{
+	int abc;
+
+	for (abc = a; abc <= z; a++)
+	{
+	printf(abc);
+	return (0);
+	}
+}	
