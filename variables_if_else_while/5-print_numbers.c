71e6cbda22c3712c8e198a6f50bdcc424c754969
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 08:51:58 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - imprime los numeros del 0 al 9
+ * 
+ * 
+ * 
+ * 
+ * Return: Always 0 (Succes)
+*/
+
+int main(void)
+{
+    int numeros;
+
+    for (numeros = 0; numeros <= 9; numeros++)
+    {
+        printf(numeros);
+    }
+    printf('\n')
+}
