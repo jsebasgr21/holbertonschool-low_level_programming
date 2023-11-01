8153e6a6929fa201aadfe6a309f68adbd2dfcd1b
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 15:15:26 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - imprime el alphabet diez veces
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int abc;
+
+	for (i = 0; i <= 10; i ++)
+	{
+	for (abc = 'a'; abc <= 'z'; abc ++)
+	{
+	putchar(abc);
+	}
+	putchar('\n');
+	}
+}
