d7003b2c32b3af083f618066287191d3fcb60ed7
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 08:33:37 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - print the abecedary without q and e
+ *
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		if (abc != 'q' && abc != 'e')
+		{
+		puthcar(abc);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
