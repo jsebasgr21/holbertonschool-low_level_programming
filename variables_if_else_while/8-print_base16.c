23920e8888699a400b4cbe2b7f17f77566a6c496
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 11:38:04 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - all numbers base 16 in lowercase
+ *
+ *
+ *
+ *
+ * Return: Always 0 (Succes)
+*/
+int main(void)
+{
+	int seis;
+
+	for (seis = 0; seis <= 'f'; seis++)
+	{
+	putchar(seis);
+	}
+	putchar('\n');
+	return (0);
+}
