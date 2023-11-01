df00c7cd65f57167f17f902a3884ad434d19baaa
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-01 08:11:38 -0700

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
+/**
+ * main - imprime el alfabeto
+ *
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char abc;
 	for (abc = 'a'; abc <= 'z'; abc++)
 	{
-		putchar(abc);
+	putchar(abc);
 	}
 	putchar('\n');
 	return (0);
