78e6ccfbd57265adc741c13dc5f5d15b2eb18399
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-02 11:43:11 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - imprime el ultimo digito de un numero
+ * @n: el numero al que se le imprimira el ultimo numero
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	printf(last_digit);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+int print_last_digit(int);
 int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
