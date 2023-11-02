224147bb7fcb0751300a64f3072cddb219bda076
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-02 11:33:16 -0700

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _abs - imprime el valor absoluto
+ * @n: el numero a imprimir absoluto
+ * Return: -n si es negativo para dar su absoluto y num para el mismo valor
  */
 int _abs(int n)
 {
