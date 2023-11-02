2662f166bdcc4ebfdfd61877da6291ed3227e65d
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-02 09:19:10 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,15 +9,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	putchar('+');
-	putchar('1');
-	return(1);
+	return (1);
 	}
 	else if (n == 0)
 	{
+	putchar('0');
 	return (0);
 	}
 	else
 	{
+	putchar('-');
 	return (1);
 	}
 }
