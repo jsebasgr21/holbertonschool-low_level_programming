b40806981bca7a1d9f6f4d5e95119d9cf93b4aef
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 13:23:54 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplica dos integrer
+ * @a: primer numero
+ * @b: segundo numero
+ * Return: retorna la multiplicacion de a y b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
