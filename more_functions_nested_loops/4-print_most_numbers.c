0275632757a0bfe51cf009ba76383e6495ec7485
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 13:52:31 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - imprime los numeros del 0 al 9
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
