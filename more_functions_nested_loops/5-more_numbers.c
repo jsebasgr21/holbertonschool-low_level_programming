95f19302e11d21012b0059759beae0bc2bc6e008
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 14:14:09 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - imprime los numeros del 0 al 14 diez veces
+ */
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar('0');
+	}
+	for (n = '1'; n <= '14'; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
