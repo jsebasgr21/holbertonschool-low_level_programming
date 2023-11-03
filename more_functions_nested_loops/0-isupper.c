2f6ae5764c8ef961eba7737c1dd9256205baf153
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 12:53:36 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - detecta si una letra esta o no esta en lower case
+ * @c: caracteres a ser chequeados
+ * Return: si es 1 es lower si es 0 no es lower.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	return (1);
+	}
+	else
+	{
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	return (0);
+	}
+}
