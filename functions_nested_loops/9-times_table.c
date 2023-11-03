57eb3cead0cf4cb914de501b650caa5c874d910f
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-02 20:54:27 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,12 +12,13 @@ void times_table(void)
 	_putchar(i);
 	_putchar(',');
 	_putchar(' ');
-	}
+
 	for (n = '1'; n <= '9'; n++)
 	{
-	_putchar(n * '9');
+	_putchar(i * n);
 	_putchar(',');
 	_putchar(' ');
 	}
 	_putchar('\n');
+	}
 }
