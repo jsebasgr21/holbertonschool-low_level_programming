55ff5fb7e4bd85707811c1778505e0652cea7ee0
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-02 21:28:37 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@ void times_table(void)
 {
 	int i;
 	int n;
-
+	int result = i * n;
 	for (i = 48; i <= 57; i++)
 	{
 	_putchar(48);
@@ -16,7 +16,7 @@ void times_table(void)
 
 	for (n = 49; n <= 57; n++)
 	{
-	_putchar(i);
+	_putchar(result);
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
