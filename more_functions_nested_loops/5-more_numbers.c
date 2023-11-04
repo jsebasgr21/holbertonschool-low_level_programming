89ca6da6765ea3be5ba7b8a83fc5320393b7bdfc
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 19:27:41 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 	{
 	for (n = 0; n <= 14; n++)
 	{
-		_putchar((n % 10) + '0');
+		_putchar((n / 10) + '0');
 
 		if (n >= 10 && n < 14)
 		{
-			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 	}
 	_putchar('\n');
