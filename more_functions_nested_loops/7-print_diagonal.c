0353ee1320dfa2e28443eb90e981742691fab1c9
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 20:43:19 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - imprime _ la cantidad de veces que sea n
+ * @n: es la cantidad de veces que imprimira el _
+ */
+void print_diagonal(int n)
+{
+	int e;
+	int s;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (e = 0; e < n; e++)
+	{
+		for (s = 1; s < n; s++
+				{
+				_putchar(' ');
+				}
+		_putchar('\\');
+	}
+	_putchar('\n');
+	}
+}
