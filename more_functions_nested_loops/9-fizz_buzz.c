27ceff46a9ce19903faee34698db8db54f67683c
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-03 21:19:01 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - imprime back slash en la terminal
+ * @n: es la cantidad de veces que imprimira el back slash
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ( i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		if ( i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf(i);
+		}
+	}
+}
