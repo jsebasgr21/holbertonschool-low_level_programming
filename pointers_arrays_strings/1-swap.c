bd9e8adf467367952d9d0b9d99daba0a38808a6d
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-06 10:41:24 -0800

    cambio

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * swap_int - cambia los valores de dos integrers
+ * @*a: valor de a en main
+ * @*b: valor de b en main
+ */
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
