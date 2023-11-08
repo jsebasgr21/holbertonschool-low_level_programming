e8a8e3db92e15e526902f106ead06e50e7bd54e9
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-08 10:02:06 -0800

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: la string a poner al revez
+ * Return: retorna la string al revez
+ */
+
+void rev_string(char *s)
+{
+	int patras = 0;
+
+	while (s[patras] != '\0')
+	{
+		patras++;
+	}
+	for (; s[patras] >= 0; patras--)
+	{
+		_putchar(s[patras]);
+	}
+}
