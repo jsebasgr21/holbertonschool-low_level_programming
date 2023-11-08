3e12abb45dc3f0239bf00ace5d27f5025aed1129
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-08 15:56:59 -0800

    cambio

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,8 @@
 
 void rev_string(char *s)
 {
-/* la variale len sera el tamaño de la string
- * la variable i sera la string a imprimir al revez
- */
+/* la variale len sera el tamaño de la string la variable i sera la string a imprimir al revez*/
+
 	int len = 0;
 	int i = len - 1;
 	int aux = 0;
