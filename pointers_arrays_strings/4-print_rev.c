4ade0b8ff39c6081cc971cfbf0cd0d949cb92693
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-07 16:34:33 -0800

    cambio

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,8 @@ void print_rev(char *s)
 	int largo = 0;
 	int i;
 
-	while (s[largo++])
-		i++;
-	for (largo = i - 1; i >= '0'; i--)
+	while (s[i++])
+		largo++;
+	for (i = largo - 1; i >= '0'; i--)
 		_putchar(s[i]);
 }
