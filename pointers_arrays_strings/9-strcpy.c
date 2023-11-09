344e4f456b025cca1211a598c2f949f775407c12
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-09 14:36:12 -0800

    cambio

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int aux = 0;
+
+	while (src[aux] != src[aux - 1])
+	{
+		dest[aux] = src[aux];
+		aux++;
+	}
+	dest[aux] = '\0';
+	return (dest);
+}
