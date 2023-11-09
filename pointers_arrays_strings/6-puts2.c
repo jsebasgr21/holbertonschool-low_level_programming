ea5c829d137686433d00c8d3081137ed20fb10ee
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-08 17:02:36 -0800

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * puts2 -  prints very other character of a string,
+ * @str: - la string a imprimir
+ */
+void puts2(char *str)
+{
+	int largo = 0;
+	int i = 0;
+	char *pc = str;
+	int o;
+
+/**
+ * mientras *pc sea diferente a \0 entonces pc++ y la variable
+ * largo++ tambien aumenten en 1, esto hace que pc y la variable
+ * largo esten al final de la string
+ */
+	while (*pc != '\0')
+	{
+	pc++;
+	largo++;
+	}
+/**
+ * i es igual a largo - 1 significa que i se posiciona un caracter antes
+ * del caracter \0
+ */
+	i = largo;
+/**
+ * la variable o mientras sea menor o igual a la varible i
+ * que se encuentra un caracter antes de \0 va a aumentar en
+ * uno su valor
+ */
+	for (o = 0; o <= i; o++)
+	{
+/**
+ * si modulo 2 de o, osea que si o es divisible por 2, entonces
+ * putchar el valor de la posicion en donde se encuentra o
+ * de lo contrario pasa al siguiente pero sin imprimirlo
+ */		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
