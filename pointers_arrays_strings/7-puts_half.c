e6861d1a0704707e3aa332a17aa58d01c788d616
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-08 17:46:52 -0800

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half -  prints half of a string, followed by a new line.
+ * @str: la string a imprimir
+ */
+void puts_half(char *str)
+{
+	char *pc = str;
+	int largo = 0;
+	int aux;
+
+	while (*pc != '\0')
+	{
+		pc++;
+		largo++;
+	}
+	aux = largo / 2;
+	for (; aux <= '\0'; aux ++)
+	{
+	_putchar(str[aux]);
+	}
+}
