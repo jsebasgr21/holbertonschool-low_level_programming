d913dbbe4be2df98ad7bdc70dabbd55ca225c3bd
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-09 15:31:33 -0800

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: la string a ser convertida en integrers
+ */
+int _atoi(char *s)
+{
+	int num = atoi(s);
+
+	printf("%d\n", num);
+	return 0;
+}
