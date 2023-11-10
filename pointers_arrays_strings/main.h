7429d35dad82ec4ecfa4ad9f613a299de53bb901
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 15:47:59 -0800

    cambio

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 void reset_to_98(int *n);
