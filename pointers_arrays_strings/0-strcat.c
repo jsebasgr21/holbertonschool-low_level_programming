324983a20c36e8033ab7f1c6c6d19ac106cea6c7
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 12:49:59 -0800

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: la variable puntero tipo char
+ * @src: variable puntero de tipo char
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int d = 0;
+	int s = 0;
+
+	while (dest[d] != '\0')
+		d++
+
+	for (; src[s] != '\0'; s++, d++)
+	{
+		dest[d] = src[s];
+	}
+	dest[d] = '\0';
+	return (dest);
+}
