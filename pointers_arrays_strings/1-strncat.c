66202962fc87dd51484ea023ca801db93c5c7362
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 13:43:52 -0800

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (; src[j] = n; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
+		
+
