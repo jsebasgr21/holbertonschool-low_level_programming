2ca0d1617c3ed464abdee56770fe6b2c495c8973
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 16:00:59 -0800

    cambio

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * _strncpy - copia una string en otra variable
+ * @dest: variable puntero de tipo char
+ * @src: variable puntero de tipo char
+ * @n: el numero de caraceres a copiar de una variable a otra
+ * Return: retorna el valor de la variable auxiliar tmp
  */
 char *_strncpy(char *dest, char *src, int n)
 {
