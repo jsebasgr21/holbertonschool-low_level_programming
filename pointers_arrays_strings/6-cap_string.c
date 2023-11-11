61c12a403ff1f16dc3c8656a1389427adf134224
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 19:59:56 -0800

    cambio

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
-	if (str[i - 1] == ' ' ||
+		if (str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||
@@ -30,7 +30,6 @@ char *cap_string(char *str)
 		str[i - 1] == '}' ||
 		i == 0)
 		str[i] -= 32;
-
 		i++;
 	}
 
