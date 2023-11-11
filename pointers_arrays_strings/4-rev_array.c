73719f3c508151223f8b634dadca0620df731bfe
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 17:46:05 -0800

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		if (i != '\0')
+		{
+		printf(", ");
+		}
+		printf("%d", a[i]);
+		i--;
+	}
+	printf("\n");
+}
