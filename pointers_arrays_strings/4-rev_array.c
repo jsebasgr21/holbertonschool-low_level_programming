9d10a959c358e9089f293966b860df061e4538df
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 19:35:39 -0800

    cambio

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -4,16 +4,16 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0;
-	int *ad = a;
-
-	while (ad[i] != '\0')
+	int j = 0;
+	int ad[99];
+	
+	while (i < n)
+    {
+        ad[i] = a[i];
+	i++;
+    }
+	for (i = i - 1; i >= 0; i--, j++)
 	{
-		i++;
+		a[j] = ad[i];
 	}
-	while (ad[i] >= n)
-	{
-		i--;
-		a[i] = ad[i];
-	}
-	_putchar('\n');
 }
