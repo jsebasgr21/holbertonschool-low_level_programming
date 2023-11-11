e183e8ebec66f9a49f71484e54c6a0bb5d0db696
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 19:48:21 -0800

    cambio

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
+ * reverse_array - revierte una array
+ * @a: la array de origen
+ * @n: el numero de integrers a copiar
  */
 void reverse_array(int *a, int n)
 {
 	int i = 0;
 	int j = 0;
 	int ad[99];
-	
+
 	while (i < n)
-    {
-        ad[i] = a[i];
-	i++;
-    }
+	{
+		ad[i] = a[i];
+		i++;
+	}
 	for (i = i - 1; i >= 0; i--, j++)
 	{
 		a[j] = ad[i];
