3e84478fc462cbe280a22bb6dd6007af902c21d6
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-10 17:34:41 -0800

    cambio

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
