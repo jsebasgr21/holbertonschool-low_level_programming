80eda9674856b54e78aa82783580b9cf288e0476
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-13 10:03:25 -0800

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: th string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s++);
+}
---
+++ recursion/README.md
@@ -0,0 +1,5 @@
+This proyect is about recursions, C - Recursion
+What is recursion
+How to implement recursion
+In what situations you should implement recursion
+In what situations you shouldn’t implement recursion
