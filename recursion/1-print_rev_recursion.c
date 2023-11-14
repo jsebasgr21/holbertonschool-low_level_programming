24027888a53ae3b38ca77da0dd2bad24810d997f
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-13 17:50:46 -0800

    arreglos en la estructura del if

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a reverse string
+ * @s: the string to be printed in reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s+1);
+		_putchar(*s);
+	}
+}
