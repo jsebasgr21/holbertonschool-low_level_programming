0d893a8345c66cfc8906de99cb8db6cc19fa6ed1
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-14 07:38:30 -0800

    documentacino betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,8 @@
 #include "main.h"
 /**
+ * _strlen_recursion - return the length of a string
+ * @s: the string to be analized and returned the length
+ * Return: return the length of the string
  */
 int _strlen_recursion(char *s)
 {
