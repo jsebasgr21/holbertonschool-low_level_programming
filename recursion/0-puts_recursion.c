41376e966a943cd14fd80fe89ae09483ad2a468a
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-13 17:12:44 -0800

    cambios en la sintaxis y orden de las variables

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,15 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s <= '\0')
 	{
+		_putchar('\n');
 		return;
 	}
-	else 
-	_putchar("%c", s[i]);
-	i++;
-	_putchar('\n');
-
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
