fb64d2f9ddbc4d1f384de77b6c6ebce0ca835531
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-15 16:51:52 -0800

    punto 4 correccion en la recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,10 +9,10 @@ int _pow_recursion(int x, int y)
 	}
 	if (y == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-	return (_pow_recursion (x ^ y));
+	return (x * _pow_recursion (x, y - 1));
 	}
 }
