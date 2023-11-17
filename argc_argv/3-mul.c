1d1abca56c6c942bf0ba96863855e7da89a12e92
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 08:07:24 -0800

    add documentation and variables num1 num2 and result

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
+ * main - multiplica dos argumentos
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: return 1 if have mor than 2 arguments or 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int resultado = num1 * num2;
+
+	if (argc == 0 && argc > 2)
 	{
-	return (*argv[1] * *argv[2]);
+		printf("error\n");
+		return (1);
 	}
+
+	printf("%d\n", resultado);
 	return (0);
 }
