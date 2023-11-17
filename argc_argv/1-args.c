7cd1e925f954fb9c18f79115e37e448bce2332c2
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-16 17:49:01 -0800

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 /**
+ * main - prints the number of arguments passed into it.
+ * @argc: argc
+ * @argv: argv
+ * Return: return 0
  */
 int main(int argc, char __attribute__((unused)) *argv[])
 {
