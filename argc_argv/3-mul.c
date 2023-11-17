3b83cf92d12ae99c654017f1428e6de2990c58ab
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 08:15:54 -0800

    test 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,15 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int resultado = num1 * num2;
-
 	if (argc != 3)
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
+
+	int num1 = atoi(argv[1]);
+        int num2 = atoi(argv[2]);
+        int resultado = num1 * num2;
 
 	printf("%d\n", resultado);
 	return (0);
