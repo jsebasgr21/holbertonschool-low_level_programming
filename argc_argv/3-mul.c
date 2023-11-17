cb423b948ac984fe29ef2bf643c586e4ab41ab84
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 08:19:32 -0800

    fix coding style

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-        num2 = atoi(argv[2]);
-        resultado = num1 * num2;
+	num2 = atoi(argv[2]);
+	resultado = num1 * num2;
 
 	printf("%d\n", resultado);
 	return (0);
