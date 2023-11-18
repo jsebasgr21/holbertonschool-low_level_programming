759064af08218df1cabc7aff89c20d1c0b52e937
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 19:42:33 -0800

    test

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,13 +1,42 @@
 #include "main.h"
 /**
+ * alloc_grid - Function that returns a pointer to a 2 dimensional array of
+ * integrers
+ * @width: la variable del ancho de mi array
+ * @height: la variable del alto de mi array
+ * Return: returns a pointer to a 2 dimensional array of integers.
  */
 int **alloc_grid(int width, int height)
 {
-	char *alloc;
-	int w = sizeof(width);
-	int h = sizeof(height);
-	int grid[h][w];
+	int **alloc;
+	int i, j;
 
-	alloc = grid[h][w];
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+
+	alloc = malloc(sizeof(int *) * height);
+
+	if (height == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		alloc[i] = malloc(sizeof(int) * width);
+
+		if (alloc[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+			free(alloc[j]);
+			}
+		free(alloc);
+		return (NULL);
+		}
+	}
+
 	return (alloc);
 }
