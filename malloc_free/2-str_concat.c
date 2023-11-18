ac75bfe9441089d634a588de0906559c553a9d34
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 16:56:33 -0800

    add documentation

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,11 +1,15 @@
 #include "main.h"
 #include <string.h>
 /**
+ * str_concat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * Return: return the concatenation of the 2 strings
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *con;
-	
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -15,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	con = malloc(strlen(s1)+strlen(s2)+1);
+	con = malloc(strlen(s1) + strlen(s2) + 1);
 
 	if (con == NULL)
 	{
