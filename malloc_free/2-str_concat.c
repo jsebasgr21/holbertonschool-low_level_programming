8b783d535af48dcb2cf7592025c694cbee58e75c
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 16:50:23 -0800

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,12 +6,23 @@ char *str_concat(char *s1, char *s2)
 {
 	char *con;
 	
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	con = malloc(strlen(s1)+strlen(s2)+1);
+
+	if (con == NULL)
+	{
+		free(con);
+		return (NULL);
+	}
+
 	con[0] = '\0';
 	strcat(con, s1);
 	strcat(con, s2);
