001792a50e03110fe0a1eb0684407686785e2784
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-17 18:07:37 -0800

    test

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,16 +3,9 @@
  */
 int **alloc_grid(int width, int height)
 {
-	/**
 	char *alloc;
 	int w = sizeof(width);
 	int h = sizeof(height);
 	int grid[h][w];
-	*/
-	int grid[2][2] = {
-	{1, 2},
-	{3, 4}};
-	printf(grid, 2, 2);
-	return (0);
-
-}	
+	return (grid);
+}
