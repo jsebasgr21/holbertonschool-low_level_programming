b74a2324157136a1aa9a8a618a909ed8960cc670
jsebasgr21 <jsebasgr21@hotmail.com>
2023-11-20 16:33:14 -0800

    add documentation and fix traling spaces

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
+ * malloc_checked - allocates memory using malloc.
+ * @b: unsigned int
+ * Return: return the pointer with the alocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	char *ptr;
 
 	ptr = malloc(sizeof(*ptr) * b);
-	
+
 	if (ptr == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
-	
+
 	return (ptr);
 }
