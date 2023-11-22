917d8f642664e42f3d5382dd280e12456173cab8
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-22 18:13:46 -0500

    test

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: array of elements
+ * @size: the size of bytes
+ * Return: return the pointer to the alocated memory
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    char *ptr = 0;
+
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+
+    ptr = malloc((sizeof(size) + sizeof(nmemb)) + 1);
+
+    if (ptr == 0)
+        return(0);
+
+    return (ptr);
+}
