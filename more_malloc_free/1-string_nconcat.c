2964fcf4fc026fac29ae637233a9050fb091acb4
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-22 02:11:32 -0500

    fix spaces

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,28 +12,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s11 = 0, s22 = 0, i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = ("");
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = ("");
-	}
+
 	for (; s1[s11] != '\0'; s11++)
+
 	for (; s2[s22] != '\0'; s22++)
+
+	if (n >= s22)
+		n = s22;
 	ptr = malloc((s11 + n + 1) * 1);
+
 	if (ptr == 0)
-	{
 		return (0);
-	}
+
 	for (i = 0; i < s11; i++)
-	{
 		ptr[i] = s1[i];
-	}
+
 	for (j = 0; j < n; j++)
-	{
 		ptr[i + j] = s2[j];
-	}
+
 	ptr[i + j] = '\0';
+
 	return (ptr);
 }
