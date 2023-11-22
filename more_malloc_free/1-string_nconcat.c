27ceb6d4ef9da90836f38fa2cd57179338422d3b
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-22 02:07:47 -0500

    add documentation

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * string_nconcat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bites
+ * Return: return the pointer ptr
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -10,45 +15,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = ("");
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = ("");
 	}
-
-	while (s1[s11] != '\0')
-	{
-		s11++;
-	}
-
-	while (s2[s22] != '\0')
-	{
-		s22++;
-	}
-
-	if (n >= s22)
-	{
-		n = s22;
-	}
-
+	for (; s1[s11] != '\0'; s11++)
+	for (; s2[s22] != '\0'; s22++)
 	ptr = malloc((s11 + n + 1) * 1);
-	
 	if (ptr == 0)
 	{
 		return (0);
 	}
-
 	for (i = 0; i < s11; i++)
 	{
 		ptr[i] = s1[i];
 	}
-
 	for (j = 0; j < n; j++)
 	{
 		ptr[i + j] = s2[j];
 	}
-
 	ptr[i + j] = '\0';
-
 	return (ptr);
 }
