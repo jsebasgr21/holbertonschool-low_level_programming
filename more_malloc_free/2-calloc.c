dfe018f5a5a715a92edc17ad19e1055e4aed8d70
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-22 19:15:08 -0500

    fix coding style

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,21 +8,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
-    char *ptr2;
-    unsigned int i;
+char *ptr2;
+unsigned int i;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+if (nmemb == 0 || size == 0)
+    return (NULL);
 
-    ptr = malloc(nmemb * size);
+ptr = malloc(nmemb * size);
 
-    if (ptr == NULL)
-        return(NULL);
+if (ptr == NULL)
+    return(NULL);
 
-    ptr2 = ptr;
+ptr2 = ptr;
 
-    for (i = 0; i <= nmemb * size; i++)
-        ptr2[i] = 0;
+for (i = 0; i < nmemb * size; i++)
+    ptr2[i] = 0;
 
-    return (ptr);
+return (ptr);
 }
