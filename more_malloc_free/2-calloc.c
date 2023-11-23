d3f367a4cc01166f743f5bbf6ef28e45aaff00d4
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-22 19:20:20 -0500

    test 5

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,17 +12,17 @@ char *ptr2;
 unsigned int i;
 
 if (nmemb == 0 || size == 0)
-    return (NULL);
+return (NULL);
 
 ptr = malloc(nmemb * size);
 
 if (ptr == NULL)
-    return(NULL);
+return(NULL);
 
 ptr2 = ptr;
 
 for (i = 0; i < nmemb * size; i++)
-    ptr2[i] = 0;
+ptr2[i] = 0;
 
 return (ptr);
 }
