0e5baac696064c308134af135e3d168c527e3ced
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-24 16:17:22 -0500

    test 3

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,14 +11,14 @@ if (d)
 if (!(d->name))
 printf("Name: (nil)\n");
 else
-printf("Name : %s\n", d->name);
+printf("Name: %s\n", d->name);
 
-printf("Age : %f\n", d->age);
+printf("Age: %f\n", d->age);
 
 if (!(d->owner))
 printf("Owner: (nil)\n");
 else
-printf("Owner : %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 }
-printf("\n");
+
 }
