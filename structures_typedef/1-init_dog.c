abf04f22c13ed5ac763b4d0ca3e0b31b073c6042
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-24 15:11:27 -0500

    test documentation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * init_dog -  initialize a variable of type struct dog
+ * @d: struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
