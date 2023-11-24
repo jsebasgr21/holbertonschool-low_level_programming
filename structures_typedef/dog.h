8783a8f35ae9e7008f4dbe4c0f977ace1cfc24e6
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-24 14:34:29 -0500

    test documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - new type struct dog
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
+
+#endif
