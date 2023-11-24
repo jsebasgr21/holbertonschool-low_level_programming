b17d36ea446526edc53e1862c4077549db07bbff
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-23 22:12:03 -0500

    add documentation

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * array_range -  creates an array of integers.
+ * @min: variable with integers
+ * @max: variable with integers
+ * Return: pointer with an array of integers
+ */
+int *array_range(int min, int max)
+{
+int *ptr;
+int i;
+
+if (min > max)
+return (NULL);
+
+ptr = malloc(sizeof(int) * (max - min + 1));
+
+if (ptr == NULL)
+return (NULL);
+
+for (i = 0; min <= max; i++, min++)
+ptr[i] = min;
+
+return (ptr);
+}
