15efbe27b093852bc104af4f1b6b319804c93704
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-24 21:35:37 -0500

    test 2 conditional

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
 
+if (array == NULL && action == NULL)
+action(*array);
+
 for (i = 0; i < size; i++)
 {
 action(array[i]);
