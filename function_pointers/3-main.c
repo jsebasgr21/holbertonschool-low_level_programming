47cfd1d53bce5bfaf4bd4e235291c97bef7a76da
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-11-24 23:58:39 -0500

    test

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,26 +15,26 @@
 
 int main(int argc, char *argv[])
 {
-	char *op;
-	int num1, num2;
+char *op;
+int num1, num2;
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-	op = argv[2];
+op = argv[2];
 
-	if (op == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+if (op == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
-	return (0);
+printf("%d\n", get_op_func(op)(num1, num2));
+return (0);
 }
