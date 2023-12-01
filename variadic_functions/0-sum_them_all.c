f1bdecff02baf1f08804a3fdd7ed586f5248230d
jsebasgr21 <jsebasgr21@DESKTOP-9GHKSO3>
2023-12-01 15:30:02 -0500

    va_list

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * sum_them_all - returns the sum of all its parameters.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+int i;
+if (n == 0)
+{
+return (0);
+}
+
+va_list args;
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+return (0);
+}
