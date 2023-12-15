15a2bf2e866a8c8377f537d6c92159c514223e06
jsebasgr21 <jsebasgr21@hotmail.com>
2023-12-15 15:23:52 -0500

    return n

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- * 
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: n
 */
 size_t print_list(const list_t *h)
 {
@@ -15,5 +17,5 @@ printf("[%d] %s\n", h->len, h->str);
 h=h->next;
 n++;
 }
-return(h);
+return(n);
 }
