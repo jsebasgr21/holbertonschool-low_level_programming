50375360399d20680620f1be5375f3a73bebbeed
jsebasgr21 <jsebasgr21@hotmail.com>
2023-12-15 22:52:14 -0500

    test

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 /**
-*
+* add_dnodeint - adds a new node at the beginning of a dlistint_t list
+* @head: head of the list
+* @n: value of the element
+* Return: the address of the new element, or NULL if it failed
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
