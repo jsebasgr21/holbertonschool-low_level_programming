3e1f9af8cea4503e44b4120f769fff7ad06ccc50
jsebasgr21 <jsebasgr21@hotmail.com>
2023-12-15 22:45:23 -0500

    test1

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+*
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *nnode = malloc(sizeof(dlistint_t));
+
+if (nnode = NULL)
+return (NULL);
+
+nnode->n = n;
+nnode->prev = NULL;
+nnode->next = *head;
+
+if (*head != NULL)
+(*head)->prev = nnode;
+
+*head = nnode;
+return (nnode);
+}
