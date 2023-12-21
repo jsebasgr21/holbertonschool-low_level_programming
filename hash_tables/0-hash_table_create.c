982c19ed1f0907fd4ec5cfe40457144fb3f97e45
jsebasgr21 <jsebasgr21@hotmail.com>
2023-12-21 18:30:56 -0500

    test1

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+* hash_table_create - Creates a hash table.
+* @size: The size of the array.
+* Return: A pointer to the newly created hash table
+* or NULL if an error occurred.
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_node_t **h_node;
+hash_table_t *h_t = NULL;
+
+if (size == 0)
+return NULL;
+
+h_t = malloc(sizeof(hash_table_t));
+if (h_t == NULL)
+return NULL;
+
+h_t->array = malloc(size * sizeof(hash_node_t *));
+if (h_t->array == NULL)
+{
+free(h_t);
+return NULL;
+}
+
+h_t->size = size;
+return (h_t);
+}
