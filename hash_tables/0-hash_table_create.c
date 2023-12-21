3e94a6fa4954c3835fa0fd256ae44b86692a7154
jsebasgr21 <jsebasgr21@hotmail.com>
2023-12-21 18:32:47 -0500

    test3

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,17 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *h_t = NULL;
 
 if (size == 0)
-return NULL;
+return (NULL);
 
 h_t = malloc(sizeof(hash_table_t));
 if (h_t == NULL)
-return NULL;
+return (NULL);
 
 h_t->array = malloc(size * sizeof(hash_node_t *));
 if (h_t->array == NULL)
 {
 free(h_t);
-return NULL;
+return (NULL);
 }
 
 h_t->size = size;
