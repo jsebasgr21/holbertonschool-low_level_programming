b9951be3f5c4b9c3cc318a69cc26dff9e7157032
jsebasgr21 <jsebasgr21@hotmail.com>
2024-01-19 11:21:53 -0500

    test

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
